6c7196b491b8fa164354ae2b8ea7be46f2bec8f9
mataraki <mataraki@users.noreply.github.com>
2020-11-20 14:21:52 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -8,19 +8,19 @@ int main(void)
     {
         n = get_int("Height: ");
     }
-    while(n < 1 || n > 8);
+    while (n < 1 || n > 8);
     for (int i = 1; i <= n; i++)
     {
         for (int j = 0; j < n - i; j++)
         {
             printf(" ");
         }
-        for(int k = 0; k < i; k++)
+        for (int k = 0; k < i; k++)
         {
             printf("#");
         }
         printf("  ");
-        for(int l = 0; l < i; l++)
+        for ./(int l = 0; l < i; l++)
         {
             printf("#");
         }
