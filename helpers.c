a89709df55838d6d90c0f4c2059e6e89695222dd
mataraki <mataraki@users.noreply.github.com>
2020-12-17 18:44:35 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -20,6 +20,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j <= width / 2; j++)
+        {
+            int tempred = image[i][j].rgbtRed;
+            image[i][j].rgbtRed = image[i][width-j+1].rgbtRed;
+            image[i][width-j+1].rgbtRed = tempred;
+            int tempblue = image[i][j].rgbtBlue;
+            image[i][j].rgbtBlue = image[i][width-j+1].rgbtBlue;
+            image[i][width-j+1].rgbtBlue = tempblue;
+            int tempgreen = image[i][j].rgbtGreen;
+            image[i][j].rgbtGreen = image[i][width-j+1].rgbtGreen;
+            image[i][width-j+1].rgbtGreen = tempgreen;
+        }
+    }
     return;
 }
 
