915c12a5af037ce1e0a22acdbcc169643f8ecdcf
mataraki <mataraki@users.noreply.github.com>
2020-12-17 19:32:20 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -46,9 +46,29 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int avgred = (int) round((double) (image[i-1][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed + image[i][j-1].rgbtRed + image[i][j].rgbtRed + image[i][j+1].rgbtRed + image[i+1][j-1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j+1].rgbtRed) / 9);
-            int avgblue = (int) round((double) (image[i-1][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i][j-1].rgbtBlue + image[i][j].rgbtBlue + image[i][j+1].rgbtBlue + image[i+1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue) / 9);
-            int avggreen = (int) round((double) (image[i-1][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i][j-1].rgbtGreen + image[i][j].rgbtGreen + image[i][j+1].rgbtGreen + image[i+1][j-1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j+1].rgbtGreen) / 9);
+            int boxes = 9;
+            if (i == 0 || i == height - 1)
+            {
+                if (j == 0 || j == width - 1)
+                {
+                    boxes = 4;
+                }
+                else
+                {
+                    boxes = 6;
+                }
+            }
+            else if (j == 0 || j == width - 1)
+            {
+                boxes = 6;
+            }
+            else
+            {
+                boxes = 9;
+            }
+            int avgred = (int) round((double) (image[i-1][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed + image[i][j-1].rgbtRed + image[i][j].rgbtRed + image[i][j+1].rgbtRed + image[i+1][j-1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j+1].rgbtRed) / boxes);
+            int avgblue = (int) round((double) (image[i-1][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i][j-1].rgbtBlue + image[i][j].rgbtBlue + image[i][j+1].rgbtBlue + image[i+1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue) / boxes);
+            int avggreen = (int) round((double) (image[i-1][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i][j-1].rgbtGreen + image[i][j].rgbtGreen + image[i][j+1].rgbtGreen + image[i+1][j-1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j+1].rgbtGreen) / boxes);
             tempimage[i][j].rgbtRed = avgred;
             tempimage[i][j].rgbtBlue = avgblue;
             tempimage[i][j].rgbtGreen = avggreen;
