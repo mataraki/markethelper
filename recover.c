1645efe4ffb69985272c6100dc30366e80dd8d72
mataraki <mataraki@users.noreply.github.com>
2020-12-18 22:34:54 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -22,31 +22,33 @@ int main(int argc, char *argv[])
     
     FILE *picture = NULL; 
     
-    int jpg_found = 0;
+    //int jpg_found = 0;
     
     while (fread(buffer, 512, 1, file) == 1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
         {
-            if (jpg_found == 1)
-            {
-                fclose(picture);
-            }
-            else
-            {
-                jpg_found = 1;
-            }
+            //if (jpg_found == 1)
+            //{
+            //    fclose(picture);
+            //}
+            //else
+            //{
+            //    jpg_found = 1;
+            //}
             
             char filename[8];
             sprintf(filename, "%03d.jpg", filecount);
             picture = fopen(filename, "a");
             filecount++;
-        }
-        
-        if (jpg_found == 1)
-        {
+            
             fwrite(&buffer, 512, 1, picture);
         }
+        
+        //if (jpg_found == 1)
+        //{
+        //    fwrite(&buffer, 512, 1, picture);
+        //}
         
     }
     fclose(file);
