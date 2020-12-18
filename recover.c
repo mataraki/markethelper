e0f130ea00b36ff8e9aea4bab2877d82c236f3ac
mataraki <mataraki@users.noreply.github.com>
2020-12-18 10:36:18 +0000

    automated commit by check50 [check50=True]

---
+++ recover.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover file\n");
+        return 1;
+    }
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Could not open %s.\n", argv[1]);
+        return 1;
+    }
+}
