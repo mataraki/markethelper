7a6d9acd96cfc25d2b4d2f3d5aaa6696388560e3
mataraki <mataraki@users.noreply.github.com>
2021-05-21 14:53:23 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -37,11 +37,17 @@ int main(int argc, char *argv[])
     uint8_t header[44];
     
     fread(header, 44, 1, input);
-    
     fwrite(header, 44, 1, output);
     
     
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
