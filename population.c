f22bdb6463e16619ca8889e89d01bf84406706fa
mataraki <mataraki@users.noreply.github.com>
2021-05-21 11:43:37 +0000

    automated commit by check50 [check50=True]

--- population.c
+++ population.c
@@ -5,25 +5,32 @@ int main(void)
 {
     // TODO: Prompt for start size
     int start;
+
     do
     {
         start = get_int("Enter the starting amount of llamas (must be 9 or more): ");
      }
     while (start < 9);
+
     // TODO: Prompt for end size
     int result;
+
     do
     {
         result = get_int("Enter the resulting amount of llamas (must not be less than starting amount): ");
      }
     while (result < start);
+
     // TODO: Calculate number of years until we reach threshold
-    int n = start;
+    int n = start, count = 0;
+
     do
     {
-        n = n + n / 3 - n / 4;
+        n += n / 3 - n / 4;
+        count += 1;
      }
     while (n < result);
+
     // TODO: Print number of years
-    printf("Years: %i!\n", n);
+    printf("Years: %i\n", count);
 }
